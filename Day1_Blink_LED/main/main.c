92136a63908871cca4de2a5a9b15aa5b47359efe
hzia <hzia8963@gmai.com>
2026-04-07 10:08:13 +0700

    Update Kconfig and Blink logic

--- Day1_Blink_LED/main/main.c
+++ Day1_Blink_LED/main/main.c
@@ -1,14 +1,13 @@
-#include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "led_driver.h"
-
-#define BLINK_GPIO GPIO_NUM_4
+#include "sdkconfig.h"
+#include <stdio.h>
 
 void app_main() {
-  led_init(BLINK_GPIO);
+  led_init(CONFIG_BLINK_GPIO);
   while (1) {
-    led_control_toggle(BLINK_GPIO);
-    vTaskDelay(pdMS_TO_TICKS(500));
+    led_control_toggle(CONFIG_BLINK_GPIO);
+    vTaskDelay(pdMS_TO_TICKS(5000));
   }
 }
